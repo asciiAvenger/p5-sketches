0425c242f89ff78a07b0a507a19c572bd38d4000
Philipp Alber <philipp_j.alber@yahoo.com>
2025-08-17 09:23:37 +0200

    switch conway-optimized sketch to use sets and no oop
    with upwards of 250k cells performance is still not
    great, so will revisit this solution

--- cellular-automata/conway-optimized/sketch.js
+++ cellular-automata/conway-optimized/sketch.js
@@ -1,7 +1,7 @@
-let cells = new Map();
+let cells;
 
-const rows = 200;
-const cols = 200;
+const rows = 500;
+const cols = 500;
 
 let cellW;
 let cellH;
@@ -9,48 +9,97 @@ let cellH;
 function setup() {
   createCanvas(1000, 1000);
 
+  cells = new Set();
+
   cellW = width / cols;
   cellH = height / rows;
 
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < cols; c++) {
       if (floor(random(2)) > 0) {
-        const key = hashKey(r, c);
-        const cell = new Cell(r, c);
-        cells.set(key, cell);
+        cells.add(toKey(r, c, cols));
       }
     }
   }
 }
 
-function hashKey(row, col) {
-  return (row << 10) | col;
-}
-
 function draw() {
   background(0);
+  fill(255);
 
-  const cellCandidates = new Map();
+  const cellCandidates = new Set();
 
-  for (const cell of cells.values()) {
+  for (const cellKey of cells.values()) {
+    const [row, col] = toRowCol(cellKey, cols);
+
     // draw currently alive cells
-    cell.draw();
+    rect(col * cellW, row * cellH, cellW, cellH);
 
     // add cell candidates for next iteration
-    cell.addCellCandidates(cellCandidates);
+    addCellCandidates(row, col, cols, cellCandidates);
   }
 
-  const nextCells = new Map();
+  const nextCells = new Set();
 
-  for (const cell of cellCandidates.values()) {
+  for (const cellKey of cellCandidates.values()) {
+    const [row, col] = toRowCol(cellKey, cols);
+
     // count the cells neighbors in the current generation of cells
-    numNeighbors = cell.countNeighbors(cells);
+    const numNeighbors = countNeighbors(row, col, cols, cells);
 
-    const key = hashKey(cell.row, cell.col);
-    if (numNeighbors == 3 || (cells.has(key) && numNeighbors == 2)) {
-      nextCells.set(key, cell);
+    if (numNeighbors == 3 || (cells.has(cellKey) && numNeighbors == 2)) {
+      nextCells.add(cellKey);
     }
   }
 
   cells = nextCells;
+}
+
+function toKey(row, col, numCols) {
+  return col + row * numCols;
+}
+
+function toRowCol(key, numCols) {
+  const row = floor(key / numCols);
+  const col = key % numCols;
+  return [row, col];
+}
+
+function addCellCandidates(row, col, numCols, candidates) {
+  for (let dr = -1; dr <= 1; dr++) {
+    for (let dc = -1; dc <= 1; dc++) {
+      // don't add the cell itself
+      if (dr == 0 && dc == 0) {
+        continue;
+      }
+
+      const r = row + dr;
+      const c = col + dc;
+      const key = toKey(r, c, numCols);
+
+      candidates.add(key);
+    }
+  }
+}
+
+function countNeighbors(row, col, numCols, cells) {
+  let sum = 0;
+
+  for (let dr = -1; dr <= 1; dr++) {
+    for (let dc = -1; dc <= 1; dc++) {
+      // don't count the cell itself
+      if (dr == 0 && dc == 0) {
+        continue;
+      }
+
+      const r = row + dr;
+      const c = col + dc;
+      const key = toKey(r, c, numCols);
+      if (cells.has(key)) {
+        sum += 1;
+      }
+    }
+  }
+
+  return sum;
 }
